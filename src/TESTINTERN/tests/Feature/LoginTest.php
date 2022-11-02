a8bf602c092ef888d3bc991b43b58f6fbddae7ef
tonvanhiep <tonvanhiepdragon@gmail.com>
2022-11-02 13:09:06 +0700

    2/11 --v1

--- src/TESTINTERN/tests/Feature/LoginTest.php
+++ src/TESTINTERN/tests/Feature/LoginTest.php
@@ -13,10 +13,41 @@ class LoginTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+
+    public function testUserCanViewLogin()
     {
-        $response = $this->get('/');
+        $response = $this->get('/admin/login');
 
         $response->assertStatus(200);
+        $response->assertViewIs('admin.login')->assertSee('ログイン');
+    }
+
+    public function testLoginPost(){
+
+        $response = $this->call('POST', '/admin/login', [
+            'email' => 'hiepvanton@quanly.com',
+            'pass' => '1234567890',
+            '_token' => csrf_token()
+        ]);
+        $response->assertRedirect('/admin/product');
+    }
+
+    public function testUserCannotLoginWithIncorrectPassword()
+    {
+        // $user = factory(User::class)->make([
+        //     'password' => bcrypt('laravel'),
+        // ]);
+
+        $response = $this->call('POST', '/admin/login', [
+            'email' => 'hiepvanton@quanly.com',
+            'pass' => '1234567890',
+            '_token' => csrf_token()
+        ]);
+        $response->assertRedirect('/admin/login');
+        // $response->assertRedirect('/admin/login');
+        // $response->assertSessionHasErrors();
+        // $this->assertFalse(session()->hasOldInput('email'));
+        // $this->assertFalse(session()->hasOldInput('pass'));
+        // $this->assertGuest();
     }
 }
